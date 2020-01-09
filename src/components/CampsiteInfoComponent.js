660a3d1a7bae9be14eac396ba7575cdb6c0df634
Huey Ru Strauss <hueyru.strauss@gmail.com>
2020-01-08 21:55:29 -0600

    functional components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from "react";
 import {
   Card,
@@ -64,6 +65,70 @@ class CampsiteInfo extends Component {
       return <div />;
     }
   }
+}
+
+export default CampsiteInfo;
+*/
+
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCampsite({campsite}) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg width="100%" top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+function RenderComments({comments}) {  
+  if (comments) {
+    return (
+      <div className="col-md-5 m-1">
+        <h4>Comments</h4>
+
+        {comments.map(comment => {
+          return (
+            <div key={comment.id}>
+              <div> {comment.text} </div>
+              <div>
+                --{comment.author}{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit"
+                }).format(new Date(Date.parse(comment.date)))}
+              </div>
+              <br></br>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+
+}
+
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
+                </div>
+            </div>
+        );
+    }
+    return <div />;
 }
 
 export default CampsiteInfo;
