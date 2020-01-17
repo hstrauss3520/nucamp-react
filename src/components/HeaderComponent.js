cc8431b31f5e4444035a0f7b8f04c1aa4f2f29d1
Huey Ru Strauss <hueyru.strauss@gmail.com>
2020-01-16 22:34:02 -0600

    Uncontrolled Form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -9,15 +10,34 @@ class Header extends Component {
         this.toggleNav = this.toggleNav.bind(this);
 
         this.state = { 
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
+
+        this.handleLogin = this.handleLogin.bind(this);
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
     }
+
+    handleLogin(event) {
+        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+        this.toggleModal();
+        event.preventDefault();
+    }
+
 
     toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
     }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -31,6 +51,33 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                            <Form onSubmit={this.handleLogin}>
+                                    <FormGroup>
+                                        <Label htmlFor="username">Username</Label>
+                                        <Input type="text" id="username" name="username"
+                                            innerRef={input => this.username = input} />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label htmlFor="password">Password</Label>
+                                        <Input type="password" id="password" name="password"
+                                            innerRef={input => this.password = input} />
+                                    </FormGroup>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox" name="remember"
+                                                innerRef={input => this.remember = input} />
+                                            Remember me
+                                        </Label>
+                                    </FormGroup>
+                                    <Button type="submit" value="submit" color="primary">Login</Button>
+                                </Form>            
+                    </ModalBody>
+                </Modal>
+
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
                     <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
@@ -58,6 +105,14 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+
+                            <span className="navbar-text ml-auto">
+                                <Button outline onClick={this.toggleModal}>
+                                    <i className="fa fa-sign-in fa-lg" /> Login
+                                </Button>
+                            </span>
+
+
                         </Collapse>
                     </div>
                 </Navbar>
