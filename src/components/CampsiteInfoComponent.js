63c33e151069d6c716a1b57d0e8dcc250fad8250
Huey Ru Strauss <hueyru.strauss@gmail.com>
2020-01-19 10:41:50 -0600

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,6 +7,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 import { Link } from 'react-router-dom';
 import { controls } from 'react-redux-form';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -177,6 +179,29 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
+
     if (props.campsite) {
         return (
             <div className="container">
